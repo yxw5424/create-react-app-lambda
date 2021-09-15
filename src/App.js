b1f8ca2e88ec93e583804f66b5e5675d793c9414
yufan wang <yxw5424@hotmail.com>
2021-09-15 19:23:14 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
-
-// class LambdaDemo extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = { loading: false, msg: null }
-//   }
-
-//   handleClick = api => e => {
-//     e.preventDefault()
-
-//     this.setState({ loading: true })
-//     fetch("/.netlify/functions/" + api)
-//       .then(response => response.json())
-//       .then(json => this.setState({ loading: false, msg: json.msg }))
-//   }
-
-//   render() {
-//     const { loading, msg } = this.state
-
-//     return (
-//       <p>
-//         <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-//         <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-//         <br />
-//         <span>{msg}</span>
-//       </p>
-//     )
-//   }
-// }
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            test test
-          </p>
-          
-        </header>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Yufan Wang</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
-    )
+    );
   }
 }
 
