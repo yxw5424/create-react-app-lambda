9dcdd440e50e53cb7c09e2821f1f4209eda01c5a
yufan wang <yxw5424@hotmail.com>
2021-09-16 03:35:14 -0400

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer(props) {
     return(
     <div className="footer">
         <div className="container">
-            <p>this is footer</p>
+            <p>this is footesssssr</p>
             {/* <div className="row justify-content-center">             
                 <div className="col-4 offset-1 col-sm-2">
                     <h5>Links</h5>
