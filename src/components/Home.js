f7537ee922cb7c1bde24808bcfd39b9236f99ea3
yufan wang <yxw5424@hotmail.com>
2021-09-16 14:46:28 -0400

    jumbo

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,21 @@ import React from 'react';
 function Home(props) {
     return(
       <div className="container">
-        
+         <Jumbotron>
+            {/* <div className="container" style={{marginTop:"80px", marginBottom:'40px'}}> */}
+            <div className="container" >
+                <div className="row row-header">
+                    <div className="col-12 col-sm-8" style={{textAlign:"left"}}>
+                        <div className="container welcome">
+                            <h1>Hello, I'm Tim.</h1>
+                            <h1>A Enginner & Designer</h1>
+                            <h1>Welcome to my Portfolio!</h1>
+                            <p>Currently available for select projects, collaborations and consulting </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Jumbotron>
         <h1>home</h1>
       </div>
       
