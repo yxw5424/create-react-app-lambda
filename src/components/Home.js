630d31328e0a0df2bdd6f88fb897d7aa60d40917
yufan wang <yxw5424@hotmail.com>
2021-09-16 14:45:06 -0400

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Hero from './Hero';
+
 
 function Home(props) {
     return(
       <div className="container">
-        <Hero/>
+        
         <h1>home</h1>
       </div>
       
