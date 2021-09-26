b51d6d37c0dcd7a1cdb5e4c3b8708fe2591c56ed
yufan wang <yxw5424@hotmail.com>
2021-09-26 13:10:37 -0400

    test galeery

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import Home from './Home';
 import Header from "./Header";
 import Footer from "./Footer";
-import Gallery from "./Gallery";
+import GalleryPage from "./GalleryPage";
 import About from "./About";
 import RobotProject from "./RobotProject";
 import LatticeProject from "./LatticeProject";
@@ -26,7 +26,7 @@ class Main extends Component {
         <div className='container' style={{marginTop:'80px'}} >
           <Switch >
               <Route path='/home' component={HomePage} />
-              <Route exact path='/gallery' component={() => <Gallery />} />
+              <Route exact path='/gallery' component={() => <GalleryPage />} />
               <Route exact path='/about' component={() => <About />} />
               <Route exact path='/robotproject' component={() => <RobotProject />} />
               <Route exact path='/latticeproject' component={() => <LatticeProject />} />
